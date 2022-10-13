c7e2a9923f14f75ac12d03045a21cfddece087f7
W0799009 <TP94@myscc.ca>
2022-10-13 00:52:43 -0400

    field created

---
+++ Models/Dolls.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barbie_Dolls.Models
+{
+    public class Dolls
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Material { get; set; }
+        public decimal Height { get; set; }
+        
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string SetTheme { get; set; }
+        public int AgeGroup { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
