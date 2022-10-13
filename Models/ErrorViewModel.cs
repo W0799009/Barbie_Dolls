b11def5317e92b601e0c0891fc9c8aab3d9999fa
W0799009 <TP94@myscc.ca>
2022-10-13 00:07:23 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Barbie_Dolls.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
