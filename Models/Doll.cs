5607d95949ab6d8709e39bdb558644d8fb30a2fd
W0799009 <TP94@myscc.ca>
2022-10-14 02:15:40 -0400

    Part 9 completed

--- Models/Doll.cs
+++ Models/Doll.cs
@@ -10,18 +10,43 @@ namespace Barbie_Dolls.Models
     public class Doll
     {
         public int ID { get; set; }
+
+        [StringLength(45, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
+
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Material { get; set; }
+
+
+        [Range(1, 40  )]
         public decimal Height { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(20)]
         public string SetTheme { get; set; }
+
+        [Range(0, 15)]
+
         public int AgeGroup { get; set; }
 
+        
+
+        [Range(1, 40)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+
+        [Range(1, 5)]
         public int Rating { get; set; }
 
     }
