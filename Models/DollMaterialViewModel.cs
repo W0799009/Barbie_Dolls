9a6dfb0a6a02c4b6bb39d3b3fec0b6c09fe8e72e
W0799009 <TP94@myscc.ca>
2022-10-13 23:50:43 -0400

    Selection list added

---
+++ Models/DollMaterialViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Barbie_Dolls.Models
+{
+    public class DollMaterialViewModel
+    {
+        public List<Doll> Movies { get; set; }
+        public SelectList Material{ get; set; }
+        public string DollMaterial{ get; set; }
+        public string SearchString { get; set; }
+    }
+}
