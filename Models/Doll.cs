5fc8dce377631196ef0ff99d07720c50c5b057e1
W0799009 <TP94@myscc.ca>
2022-10-13 22:59:42 -0400

    completed part 6

--- Models/Doll.cs
+++ Models/Doll.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Barbie_Dolls.Models
 {
@@ -12,12 +13,14 @@ namespace Barbie_Dolls.Models
         public string Name { get; set; }
         public string Material { get; set; }
         public decimal Height { get; set; }
-        
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string SetTheme { get; set; }
         public int AgeGroup { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
     }
